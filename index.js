9bb5de9faef6428141da3849a6f8e5454ec6f26a
adeilzalustosa <adeilzalustosa85@gmail.com>
2022-08-19 16:22:06 -0300

    Implement add functionality

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
-let cadastros = [
-  { id: "001", nome: "Fulano de Tal", idade: "25" },
-  { id: "002", nome: "Cumade Zé", idade: "75" },
-  { id: "003", nome: "Genoveva", idade: "102" },
-];
-cadastros.push({ id: "004", nome: "Junior", idade: "1" });
+let cadastros = [];
 
-const linhas = cadastros.map((cadastro) => {
-  const tdId = document.createElement("td");
-  tdId.classList.add("text-end");
-  tdId.innerHTML = cadastro.id;
+const fillTable = () => {
+  const linhas = cadastros.map((cadastro) => {
+    const tdId = document.createElement("td");
+    tdId.classList.add("text-end");
+    tdId.innerHTML = cadastro.id;
 
-  const tdNome = document.createElement("td");
-  tdNome.innerHTML = cadastro.nome;
+    const tdNome = document.createElement("td");
+    tdNome.innerHTML = cadastro.nome;
 
-  const tdIdade = document.createElement("td");
-  tdIdade.classList.add("text-end");
-  tdIdade.innerHTML = cadastro.idade;
+    const tdIdade = document.createElement("td");
+    tdIdade.classList.add("text-end");
+    tdIdade.innerHTML = cadastro.idade;
 
-  const tdEdicao = document.createElement("td");
+    const tdEdicao = document.createElement("td");
 
-  const editButton = document.createElement("button");
-  editButton.classList.add("btn");
-  editButton.classList.add("btn-primary");
-  editButton.type = "button";
-  editButton.innerText = "Editar";
-  tdEdicao.appendChild(editButton);
+    const editButton = document.createElement("button");
+    editButton.classList.add("btn");
+    editButton.classList.add("btn-primary");
+    editButton.type = "button";
+    editButton.innerText = "Editar";
+    tdEdicao.appendChild(editButton);
 
-  const deleteButton = document.createElement("button");
-  deleteButton.classList.add("btn");
-  deleteButton.classList.add("btn-danger");
-  deleteButton.type = "button";
-  deleteButton.innerText = "Excluir";
-  tdEdicao.appendChild(deleteButton);
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("btn");
+    deleteButton.classList.add("btn-danger");
+    deleteButton.type = "button";
+    deleteButton.innerText = "Excluir";
+    tdEdicao.appendChild(deleteButton);
 
-  const tr = document.createElement("tr");
+    const tr = document.createElement("tr");
 
-  tr.appendChild(tdId);
-  tr.appendChild(tdNome);
-  tr.appendChild(tdIdade);
-  tr.appendChild(tdEdicao);
+    tr.appendChild(tdId);
+    tr.appendChild(tdNome);
+    tr.appendChild(tdIdade);
+    tr.appendChild(tdEdicao);
 
-  return tr;
-});
+    return tr;
+  });
 
-const tabela = document.getElementById("person-list");
-linhas.forEach((linha) => tabela.appendChild(linha));
+  const tabela = document.getElementById("person-list");
+  tabela.innerHTML = "";
+  linhas.forEach((linha) => tabela.appendChild(linha));
+};
+
+const clearInputs = () => inputs.forEach((input) => (input.value = ""));
 
 let btnDanger = document.querySelector("#btn-clear");
 let inputs = document.querySelectorAll("input");
-btnDanger.addEventListener("click", () =>
-  inputs.forEach((input) => (input.value = ""))
-);
+btnDanger.addEventListener("click", clearInputs);
 
 const inputNome = document.querySelector("#nome");
 const inputIdade = document.querySelector("#idade");
@@ -66,3 +64,14 @@ const updateButtons = () => {
 
 inputNome.addEventListener("change", updateButtons);
 inputIdade.addEventListener("change", updateButtons);
+
+const saveButton = document.querySelector("#btn-save");
+saveButton.addEventListener("click", () => {
+  cadastros.push({
+    id: cadastros.length + 1,
+    nome: inputNome.value,
+    idade: inputIdade.value,
+  });
+  clearInputs();
+  fillTable();
+});
