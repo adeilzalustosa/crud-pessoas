1c15025d8513b74fea154c9a3d6bfb1a9815877f
adeilzalustosa <adeilzalustosa85@gmail.com>
2022-08-31 15:33:19 -0300

    Implement remaining functionalities

--- index.js
+++ index.js
@@ -1,77 +1,106 @@
-let cadastros = [];
+let people = [];
+
+var currentPerson = null;
+
+const inputName = document.querySelector("#name");
+const inputAge = document.querySelector("#age");
+
+const btnSave = document.querySelector("#btn-save");
+const btnClear = document.querySelector("#btn-clear");
+
+const getPersonIndex = (person) =>
+  people.findIndex((item) => item.id === person.id);
+
+const generateId = () => rando(1, 9999).toString().padStart(4, "0");
 
 const fillTable = () => {
-  const linhas = cadastros.map((cadastro) => {
+  const rows = people.map((person) => {
     const tdId = document.createElement("td");
     tdId.classList.add("text-end");
-    tdId.innerHTML = cadastro.id;
+    tdId.innerHTML = person.id;
 
-    const tdNome = document.createElement("td");
-    tdNome.innerHTML = cadastro.nome;
+    const tdName = document.createElement("td");
+    tdName.innerHTML = person.name;
 
-    const tdIdade = document.createElement("td");
-    tdIdade.classList.add("text-end");
-    tdIdade.innerHTML = cadastro.idade;
+    const tdAge = document.createElement("td");
+    tdAge.classList.add("text-end");
+    tdAge.innerHTML = person.age;
 
-    const tdEdicao = document.createElement("td");
+    const tdEdit = document.createElement("td");
 
-    const editButton = document.createElement("button");
-    editButton.classList.add("btn");
-    editButton.classList.add("btn-primary");
-    editButton.type = "button";
-    editButton.innerText = "Editar";
-    tdEdicao.appendChild(editButton);
+    const btnEdit = document.createElement("button");
+    btnEdit.classList.add("btn");
+    btnEdit.classList.add("btn-primary");
+    btnEdit.type = "button";
+    btnEdit.innerText = "Editar";
+    btnEdit.addEventListener("click", () => {
+      currentPerson = person;
+      inputName.value = person.name;
+      inputAge.value = person.age;
+    });
+    clearAll();
+    tdEdit.appendChild(btnEdit);
 
-    const deleteButton = document.createElement("button");
-    deleteButton.classList.add("btn");
-    deleteButton.classList.add("btn-danger");
-    deleteButton.type = "button";
-    deleteButton.innerText = "Excluir";
-    tdEdicao.appendChild(deleteButton);
+    const btnDelete = document.createElement("button");
+    btnDelete.classList.add("btn");
+    btnDelete.classList.add("btn-danger");
+    btnDelete.type = "button";
+    btnDelete.innerText = "Excluir";
+    btnDelete.addEventListener("click", () => {
+      const index = getPersonIndex(person);
+      if (index > -1) {
+        people.splice(index, 1);
+      }
+      fillTable();
+      clearAll();
+    });
+    tdEdit.appendChild(btnDelete);
 
     const tr = document.createElement("tr");
-
     tr.appendChild(tdId);
-    tr.appendChild(tdNome);
-    tr.appendChild(tdIdade);
-    tr.appendChild(tdEdicao);
+    tr.appendChild(tdName);
+    tr.appendChild(tdAge);
+    tr.appendChild(tdEdit);
 
     return tr;
   });
 
-  const tabela = document.getElementById("person-list");
-  tabela.innerHTML = "";
-  linhas.forEach((linha) => tabela.appendChild(linha));
+  const table = document.getElementById("person-list");
+  table.innerHTML = "";
+  rows.forEach((row) => table.appendChild(row));
 };
 
-const clearInputs = () => inputs.forEach((input) => (input.value = ""));
-
-let btnDanger = document.querySelector("#btn-clear");
-let inputs = document.querySelectorAll("input");
-btnDanger.addEventListener("click", clearInputs);
-
-const inputNome = document.querySelector("#nome");
-const inputIdade = document.querySelector("#idade");
-
-const updateButtons = () => {
-  const isValid = inputNome.value !== "" && inputIdade.value !== "";
-
-  const saveButton = document.querySelector("#btn-save");
-  const clearButton = document.querySelector("#btn-clear");
-  saveButton.disabled = !isValid;
-  clearButton.disabled = !isValid;
+const clearAll = () => {
+  currentPerson = null;
+  document.querySelectorAll("input")?.forEach((input) => (input.value = ""));
 };
 
-inputNome.addEventListener("change", updateButtons);
-inputIdade.addEventListener("change", updateButtons);
+btnClear.addEventListener("click", clearAll);
 
-const saveButton = document.querySelector("#btn-save");
-saveButton.addEventListener("click", () => {
-  cadastros.push({
-    id: cadastros.length + 1,
-    nome: inputNome.value,
-    idade: inputIdade.value,
-  });
-  clearInputs();
+const updateBtns = () => {
+  const isValid = inputName.value !== "" && inputAge.value !== "";
+  btnSave.disabled = !isValid;
+  btnClear.disabled = !isValid;
+};
+
+inputName.addEventListener("input", updateBtns);
+inputAge.addEventListener("input", updateBtns);
+
+btnSave.addEventListener("click", () => {
+  if (!currentPerson) {
+    people.push({
+      id: generateId(),
+      name: inputName.value,
+      age: inputAge.value,
+    });
+  } else {
+    const index = getPersonIndex(currentPerson);
+    if (index > -1) {
+      currentPerson.name = inputName.value;
+      currentPerson.age = inputAge.value;
+      people.splice(index, 1, currentPerson);
+    }
+  }
   fillTable();
+  clearAll();
 });
